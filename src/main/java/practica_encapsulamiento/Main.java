55150e72078691057b1b7a575283533c2100264a
Aprendiz <fabiansuez182iii@gmail.com>
2025-11-19 17:37:02 -0500

    Se actualiza la clase principal para que se vea mejor el uso de los metodos get y set.

--- src/main/java/practica_encapsulamiento/Main.java
+++ src/main/java/practica_encapsulamiento/Main.java
@@ -2,15 +2,21 @@ package practica_encapsulamiento;
 
 public class Main {
     public static void main(String[] args) {
-        CuentaBancaria cuentaBancaria = new CuentaBancaria("9638527410", "Nelson Fabián Gallego Sánchez", 1000000);
+        CuentaBancaria cuentaBancaria = new CuentaBancaria("9638527410", "Nelson Fabián", 1000000);
 
         cuentaBancaria.mostrarDatos();
+
+
+        System.out.println("Corrección de nombre del titular...");
+        cuentaBancaria.setTitular("Nelson Fabián Gallego Sánchez");
+
+        System.out.println("El titular actual es: " + cuentaBancaria.getTitular());
+
         cuentaBancaria.depositar(500000);
+
         System.out.println("Su nuevo saldo es de: " + cuentaBancaria.getSaldo());
 
         cuentaBancaria.retirar(50000);
-        cuentaBancaria.retirar(100000);
         cuentaBancaria.mostrarDatos();
     }
-
 }
